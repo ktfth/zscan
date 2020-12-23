312577a3419cbab4b0ae216e6c512d3113600bc1
Kaique da Silva <kaeyosthaeron@gmail.com>
2020-12-23 19:31:18 -0300

    Showing the IP address

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 'use strict';
 const net = require('net');
 
+function resolve(host, port, cb) {
+  return net.connect({
+    port: port,
+    host: host,
+  }, cb);
+}
+
 function isPortOpen(host, port) {
   return net.connect({
     port: port,
@@ -11,12 +18,23 @@ function isPortOpen(host, port) {
 }
 
 let args = process.argv.slice(2);
+let showedIpAddress = false;
 
 if (args.length === 2) {
-  args[1].split(/\,|\,\s/).forEach(port => {
-    const s = isPortOpen(args[0], port);
-    setTimeout(() => {
-      s.destroy();
-    }, 3000);
-  });
+  for (let port of args[1].split(/\,|\,\s/)) {
+    const s0 = resolve(args[0], port, () => {
+      if (!showedIpAddress) console.log(s0.remoteAddress);
+      const s1 = isPortOpen(s0.remoteAddress, port);
+
+      s1.setTimeout(3000);
+      s1.on('timeout', () => {
+        s1.destroy();
+      });
+    });
+
+    s0.setTimeout(3000);
+    s0.on('timeout', () => {
+      s0.destroy();
+    });
+  }
 }
